49a2fa83bde6a35c052e1f93ed01818ae0016bf9
Nate Luzod <nate.luzod@gmail.com>
2017-09-26 16:47:37 -1000

    Adding child process

---
+++ routes/fork.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const { spawn } = require('child_process')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('fork', { title: 'Forking review App' });
+});
+
+spawn('bin/fork.sh');
+
+
+module.exports = router;
